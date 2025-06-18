56b614eb8e75167f93dd6ee3b99ec004c9a2cbd9
Martin Törnros <martin.tornros@ri.se>
2025-06-18 09:36:07 +0200

    Main description

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page class="q-ma-md">
 
     <h1>{{$t('project_title')}}</h1>
+    <p>{{$t('main.description')}}</p>
     <q-btn :label="$t('itinerary.create')" to="create" unelevated color="primary"/>
 
   </q-page>
